d6b77f73a57193e80951385ef54e9a9f7ec733b8
Salva <salvi205s@gmail.com>
2019-01-31 14:25:54 +0100

    Clases con los atributos creados

--- src/empresaalquilervehiculos_salvadorsantos/EmpresaAlquilerVehiculos.java
+++ src/empresaalquilervehiculos_salvadorsantos/EmpresaAlquilerVehiculos.java
@@ -11,4 +11,28 @@ package empresaalquilervehiculos_salvadorsantos;
  */
 public class EmpresaAlquilerVehiculos {
     
+    // Atributos de la empresa
+    private String cif;
+    private String nombre;
+    private String paginaWeb;
+    
+    /* Atributos para controlar el total de clientes que tiene la
+    empresa y array de almacenamiento para los objetos Cliente */
+    private int totalCliente;
+    private Cliente[] clientes;
+    
+    /* Atributos para controlar el total de vehiculos disponibles en
+    alquiler que tiene la empresa y array de almacenamiento para los objetos
+    Vehiculo */
+    private int totalVehiculos;
+    private Vehiculo[] vehiculos;
+    
+    /* Atributos para controlar el histórico de alquileres: total de
+    alquileres realizados y array de almacenamiento para los objetos
+    vehiculoAlquilado*/
+    private int totalAlquileres;
+    private VehiculoAlquilado[] alquileres;
+    
+    
+    
 }
