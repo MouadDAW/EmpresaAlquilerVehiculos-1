8b618ec919a21f3016458de5aea30f39067ec9b9
Salva <salvi205s@gmail.com>
2019-02-01 10:52:35 +0100

    Constructores geters y seters creados

--- src/empresaalquilervehiculos_salvadorsantos/Cliente.java
+++ src/empresaalquilervehiculos_salvadorsantos/Cliente.java
@@ -14,5 +14,40 @@ public class Cliente {
     private String nif;
     private String nombre;
     private String apellidos;
+
+    public Cliente() {
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nif=" + nif + ", nombre=" + nombre + ", apellidos=" + apellidos + '}';
+    }
+    
+    
     
 }
+
