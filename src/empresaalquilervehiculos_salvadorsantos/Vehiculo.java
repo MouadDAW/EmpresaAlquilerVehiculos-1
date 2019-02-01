972b44acfb2addd766dbd7f7c77bf2b8a988eef7
Salva <salvi205s@gmail.com>
2019-02-01 10:55:04 +0100

    Constructores setters y geters creados

--- src/empresaalquilervehiculos_salvadorsantos/Vehiculo.java
+++ src/empresaalquilervehiculos_salvadorsantos/Vehiculo.java
@@ -17,6 +17,62 @@ public class Vehiculo {
     private String color;
     private double tarifa;
     private boolean disponible;
+
+    public Vehiculo() {
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", color=" + color + ", tarifa=" + tarifa + ", disponible=" + disponible + '}';
+    }
     
     
     
