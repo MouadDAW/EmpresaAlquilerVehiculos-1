2b02ea20749232ed0ad7f18851880ec58d7ff0f2
Salva <salvi205s@gmail.com>
2019-02-13 12:41:55 +0100

    Imprimir array relleno creado

--- src/empresaalquilervehiculos_salvadorsantos/MisVehiculos.java
+++ src/empresaalquilervehiculos_salvadorsantos/MisVehiculos.java
@@ -14,24 +14,24 @@ public class MisVehiculos {
     
     public static void main(String[] args) {
         EmpresaAlquilerVehiculos easydrive = new EmpresaAlquilerVehiculos("A-28-187189", "easy drive", "www.easydrive.com");
-        
-        //Registro los clientes de la empresa
-        easydrive.registrarCliente(new Cliente("X5618927C", "Juan", "González López"));
-
-        easydrive.registrarCliente(new Cliente("Z7568991Y", "Luis", "Fernández Gómez"));
-
-        // registro de los vehículos de la empresa
-        easydrive.registrarVehiculo(new Vehiculo("4060 TUR", "Skoda", "Fabia", "Blanco", 90.0, false));
-
-        easydrive.registrarVehiculo(new Vehiculo("4070 DEP", "Ford", "Mustang", "Rojo", 150.0, true));
-
-        easydrive.registrarVehiculo(new Vehiculo("4080 TUR", "VW", "GTI", "Azul", 110.0, false));
-
-        easydrive.registrarVehiculo(new Vehiculo("4090 TUR", "SEAT", "Ibiza", "Blanco", 90.0, false));
-
-        easydrive.registrarVehiculo(new Vehiculo("4100 FUR", "Fiat", "Ducato", "Azul", 80.0, true));
-        
-        
+//        
+//        //Registro los clientes de la empresa
+//        easydrive.registrarCliente(new Cliente("X5618927C", "Juan", "González López"));
+//
+//        easydrive.registrarCliente(new Cliente("Z7568991Y", "Luis", "Fernández Gómez"));
+//
+//        // registro de los vehículos de la empresa
+//        easydrive.registrarVehiculo(new Vehiculo("4060 TUR", "Skoda", "Fabia", "Blanco", 90.0, false));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4070 DEP", "Ford", "Mustang", "Rojo", 150.0, true));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4080 TUR", "VW", "GTI", "Azul", 110.0, false));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4090 TUR", "SEAT", "Ibiza", "Blanco", 90.0, false));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4100 FUR", "Fiat", "Ducato", "Azul", 80.0, true));
+//        
+//        
         // imprime la relación de clientes de easydrive
         easydrive.imprimirClientes();
 
@@ -39,6 +39,18 @@ public class MisVehiculos {
 
         // imprime el catálogo de vehículos de easydrive
         easydrive.imprimirVehiculos();
+        
+        System.out.println("----------------------------------------------------------------------");
+//        System.out.println("Aleatorios "+Cliente.clienteAleatorio());
+//        System.out.println("Aleatorios "+Vehiculo.cocheAleatorio());
+        
+        easydrive.rellenarArrayCoches();
+        easydrive.imprimirVehiculos();
+        
+        easydrive.rellenarArrayClientes();
+        easydrive.imprimirClientes();
+
+
 
     }
 }
